f45e715b403a43c582eb1111338863d3672d1462
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-25 00:30:06 -0400

    Assignment #1

---
+++ 01-drawBST.js
@@ -0,0 +1,56 @@
+// Given the data 3,1,4,6,9,2,5,7, if you were to insert this
+// into an empty binary search tree, what would the tree look
+// like? (Draw the tree, no coding needed here.)
+
+//          3
+//        /  \
+//       1    4
+//        \     \
+//         2     6
+//              / \
+//             5   9
+//                /
+//               7
+
+// Draw the BST with the keys - E A S Y Q U E S T I O N
+//
+//                  E
+//                /   \
+//              A        S
+//                     /    \
+//                    Q       Y
+//                  /        /
+//                 E       U
+//                  \     /
+//                   I    S
+//                    \    \
+//                     O    T
+//                    /
+//                   N
+
+// Show how the above trees would look
+// if you deleted the root of each tree.
+// (Draw the trees, no coding needed here.)
+
+//           4
+//         /   \
+//       1      6
+//        \    / \
+//         2   5  9
+//               /
+//              7
+
+//                  Q
+//            /         \
+//            A          S
+//              \         \
+//               I         Y
+//                \       /
+//                 O     U
+//                /     /
+//               N     T
+
+// Without running this code in your code editor,
+// explain what the following program does. Show
+// with an example the result of executing this
+// program. What is the runtime of this algorithm?
