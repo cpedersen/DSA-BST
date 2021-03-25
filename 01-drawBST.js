cc9891908c7a1e56f5a6de928f86b16c50a2face
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-25 00:31:14 -0400

    Removed unnecessary part

--- 01-drawBST.js
+++ 01-drawBST.js
@@ -27,30 +27,3 @@
 //                     O    T
 //                    /
 //                   N
-
-// Show how the above trees would look
-// if you deleted the root of each tree.
-// (Draw the trees, no coding needed here.)
-
-//           4
-//         /   \
-//       1      6
-//        \    / \
-//         2   5  9
-//               /
-//              7
-
-//                  Q
-//            /         \
-//            A          S
-//              \         \
-//               I         Y
-//                \       /
-//                 O     U
-//                /     /
-//               N     T
-
-// Without running this code in your code editor,
-// explain what the following program does. Show
-// with an example the result of executing this
-// program. What is the runtime of this algorithm?
