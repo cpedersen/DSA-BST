84297d849bbd674886df1598d525b02a1be8dc5f
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-25 22:54:25 -0400

    Assignment 4

---
+++ 04-whatDoesThisProgramDo.js
@@ -0,0 +1,28 @@
+// Without running this code in your code editor, explain
+// what the following program does. Show with an example
+// the result of executing this program. What is the
+// runtime of this algorithm?
+
+// Function accepts tree as arg
+function tree(t) {
+  // If no tree, then stop
+  if (!t) {
+    return 0;
+  }
+  // Recursively add left branch of tree function.
+  // Recursively add right branch of tree function.
+  // Return sum of tree values.
+  return tree(t.left) + t.value + tree(t.right);
+}
+
+// This program provides a total of all of the values in a tree.
+// It does this with a recursive add of all of the children from the left
+// and right subtrees of the parent.
+
+//             10
+//           /    \
+//          8      12
+//         / \     / \
+//        6   9   11  15
+
+// (6 + null = 6) + 8 + (9 + null = 9) + 10 + (11 + null = 11) + 12 + (15 + null = 15) = 71
