8906e454b7c73f51f6f808ab5f4e8b163aeb23ee
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-03-25 20:05:25 -0400

    Assignment 2

---
+++ 02-removeRoot.js
@@ -0,0 +1,68 @@
+// Before deletion of the root node
+//          3
+//        /  \
+//       1    4
+//        \     \
+//         2     6
+//              / \
+//             5   9
+//                /
+//               7
+
+// After deletion of the root node
+//          2
+//        /  \
+//       1    4
+//              \
+//               6
+//              / \
+//             5   9
+//                /
+//               7
+
+// Before deletion of the root node
+//
+//                  E
+//                /    \
+//              A        S
+//                     /    \
+//                    Q       Y
+//                  /        /
+//                 E       U
+//                  \     /
+//                   I    S
+//                    \    \
+//                     O    T
+//                    /
+//                   N
+
+// After deletion of the root node
+//                  A
+//                     \
+//                       S
+//                     /    \
+//                    Q       Y
+//                  /        /
+//                 E       U
+//                  \     /
+//                   I    S
+//                    \    \
+//                     O    T
+//                    /
+//                   N
+
+//REMOVE THE ROOT
+//
+//           E
+//        /     \
+//       A        S
+//         \     /  \
+//          E   Q     Y
+//             /  \
+//            I    \
+//             \    \
+//              O   U
+//            /   /
+//           N   S
+//              \
+//               T
